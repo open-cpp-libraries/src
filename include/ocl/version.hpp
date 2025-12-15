236f81d4f257ddaa1fff2946fe34cddc679c8997
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-15 19:00:37 +0100

    chore: new version.hpp header.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/ocl/version.hpp
@@ -0,0 +1,14 @@
+#ifndef __OCL_VERSION_HPP
+#define __OCL_VERSION_HPP
+
+// This is a Binary Encoded Decimal of the OCL's version.
+// Which will be incremented each time a new release is done.
+
+#define OCL_VERSION 0x000000
+
+// This is the library version, which applies the following pattern:
+// <branch>-<major_version> use it to detect whether you're using stable or develop OCL.
+
+#define OCL_LIB_VERSION "stable-00_"
+
+#endif
