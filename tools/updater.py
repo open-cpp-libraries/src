b316bff4178342a54f48fa6ec3bc72f0250830ee
Amlal El Mahrouss <amlal@nekernel.org>
2026-02-28 13:53:37 +0100

    chore: update updater.py
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- tools/updater.py
+++ tools/updater.py
@@ -1,16 +1,27 @@
 #! /usr/bin/python3
 
-from os import system
-
 # Copyright 2026, Amlal El Mahrouss and Ne.org contributors.
 # Open C++ Libraries is licensed under BSL-1.0
 
+from os import system
+
+class UpdateFunctor:
+    def __init__(self):
+        system("cd libs && cd tproc && git pull && cd ..")
+        system("cd libs && cd fix && git pull && cd ..")
+        system("cd libs && cd core && git pull && cd ..")
+        system("git add libs/core libs/fix libs/tproc")
+        system("git commit -s -S")
+
+
+def main():
+    functor = UpdateFunctor()
+    print("INFO: Updater: Functor called")
+
+
 if __name__ == '__main__':
-    print("precommit: running format.sh...")
-    system("cd libs && cd tproc && git pull && cd ..")
-    system("cd libs && cd fix && git pull && cd ..")
-    system("cd libs && cd core && git pull && cd ..")
-    system("git add libs/core libs/fix libs/tproc")
-    system("git commit -s -S")
+    main()
+
+
 
 
