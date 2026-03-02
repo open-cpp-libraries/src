73ff5312ab0327f2ed054f7a6e069b1f6163ce54
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-02 15:11:54 +0100

    chore: upstream fixes.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/ocl/version.hpp
+++ include/ocl/version.hpp
@@ -1,7 +1,7 @@
 // Copyright 2023-2025, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-// Official repository: https://github.com/ocl-org/ocl
+// Official repository: https://github.com/ocl-foss-org/ocl
 
 #ifndef OCL_VERSION_HPP
 #define OCL_VERSION_HPP
