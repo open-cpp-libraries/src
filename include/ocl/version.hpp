f7bd2511ef320f5f8fd672364614f6e6275b318e
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-05 15:10:36 +0100

    chore: bump OCL version.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/ocl/version.hpp
+++ include/ocl/version.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023-2025, Amlal El Mahrouss (amlal@nekernel.org)
+// Copyright 2023-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 // Official repository: https://github.com/ocl-foss-org/ocl
