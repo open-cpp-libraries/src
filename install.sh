79a98d2c0230231251b0c83f8ca0d417c665675f
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-27 12:18:12 +0100

    [CHORE] Update install.sh
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 ## To be used for: curl -fsSL https://install.ocl.nekernel.org | sh
 
+echo "==> WELCOME TO OPEN C++ LIBRARIES."
+echo "==> INSTALLING OPEN C++ LIBRARIES..."
+
+sudo apt update
+sudo apt install build-essential cmake libboost-dev
+
 git clone --recurse-submodules -j8 git@github.com:ocl-foss-org/ocl.git
 cd ocl
 cmake -B build
