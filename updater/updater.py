7da64f82e3b204945d961f784d90167adf2d436b
Amlal <amlalelmahrouss@icloud.com>
2026-04-03 01:39:58 +0200

    [CHORE] Update .vs/ to gitignore, update installer.py.
    Signed-off-by: Amlal <amlalelmahrouss@icloud.com>

--- updater/updater.py
+++ updater/updater.py
@@ -9,7 +9,7 @@ class UpdateFunctor:
         system("cd libs && cd fix && git pull && cd ..")
         system("cd libs && cd core && git pull && cd ..")
         system("git add libs/core libs/fix libs/tproc")
-        system("git commit -s -S")
+        system("git commit -s")
 
 
 def start():
