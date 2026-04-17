6213ee4382c2acab37baaa11973470cfffb68520
Amlal <amlal@nekernel.org>
2026-04-17 17:59:48 +0200

    [FEAT] src: Add `install.bat` source.

--- install.bat
+++ install.bat
@@ -1,18 +1,18 @@
-#! /bin/sh
+@echo off
 
 ## To be used for: curl -fsSL https://install.ocl.nekernel.org | sh
 
 echo "==> WELCOME TO OPEN C++ LIBRARIES."
 echo "==> INSTALLING OPEN C++ LIBRARIES..."
 
-sudo apt update
-sudo apt install build-essential cmake libboost-dev
-
 git clone --recurse-submodules -j8 https://github.com/ocl-foss/ocl.git
+
 cd ocl
 cmake -B build
+
 echo "==> INSTALLING THE OPEN C++ LIBRARIES..."
-sudo cmake --install build
+
+cmake --install build
 
 echo "==> WELCOME TO OPEN C++ LIBRARIES."
 echo "==> THANK YOU FOR USING THE OCL!"
