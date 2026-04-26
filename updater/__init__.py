491c6e5cdf743ca7f8afa79b36d043e0db7bcc88
Amlal <amlal@nekernel.org>
2026-04-26 09:08:19 +0200

    [FEAT] Update updater script.

--- updater/__init__.py
+++ updater/__init__.py
@@ -1,14 +1,18 @@
+# Copyright 2026, Amlal El Mahrouss and contributors.
+# NeSystem is licensed under Apache-2.0.
+# SPDX-Identifier: Apache-2.0
+
+# -*- coding: utf-8 -*-
+
 import updater
 
 def updater_main():
     try:
         updater.start()
-    except Error:
+    except OSError:
         print("Updater failed.")
 
 # The Upader starts here
 if __name__ == '__main__':
     updater_main()
-
-
 
