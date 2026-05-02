17fa80768002386da7853cb5a25724b5f446b3d5
Amlal <amlal@nekernel.org>
2026-05-02 14:46:06 +0200

    [CHORE] modules: Add C++23 module units for ocl, fix, and tproc

---
+++ modules/ocl/ocl.cxx
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSL-1.0
+// Copyright 2023-2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Official repository: https://git.ocl.nekernel.org/ocl
+
+export module ne_app.ocl.ocl;
+
+#include <ocl.hpp>
+
+#define _OCL_MODULE_ALL 1
