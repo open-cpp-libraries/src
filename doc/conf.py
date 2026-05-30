9e6744b4e52437cbf9a4e2756d4630b8210e52b2
Amlal <amlal@nekernel.org>
2026-05-30 19:03:34 +0200

    [FIX] src: Patch sphinx configuration file.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
-project = 'My Python Project'
+project = 'Open C++ Libraries'
 author = 'Amlal El Mahrouss'
 copyright = '2026, Amlal El Mahrouss'
 version = '1.63'
