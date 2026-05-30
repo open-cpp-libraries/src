6189893203eb2bb8a00de3203188b5e1ed3300b3
Amlal <amlal@nekernel.org>
2026-05-30 18:54:20 +0200

    [CHORE] src: update conf.py
    Signed-off-by: Amlal <amlal@nekernel.org>

--- doc/conf.py
+++ doc/conf.py
@@ -1,1 +1,12 @@
-html_theme = 'sphinx_rtd_theme'
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+
+project = 'My Python Project'
+author = 'Amlal El Mahrouss'
+copyright = '2026, Amlal El Mahrouss'
+version = '1.63'
+release = '1.63.7'
+extensions = ['sphinx.ext.markdown', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+language = 'en'
+html_theme = 'pydata_sphinx_theme'
